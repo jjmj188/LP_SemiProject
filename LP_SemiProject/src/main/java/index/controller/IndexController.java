702b08565f4277ffcc6826c1935e33b0d76f90e7
dltlg <dltlg@dltlgud>
2026-01-07 15:24:34 +0900

    상세페이지, 정렬 Merge branch 'main' of https://github.com/jjmj188/LP_SemiProject.git into main

--- LP_SemiProject/src/main/java/index/controller/IndexController.java
+++ LP_SemiProject/src/main/java/index/controller/IndexController.java
@@ -24,11 +24,18 @@ public class IndexController extends AbstractController {
 		if(strCategoryNo != null) {
 			try {
 				categoryNo = Integer.parseInt(strCategoryNo);
-			} catch(NumberFormatException e) {
-				categoryNo = 0; // 숫자가 아닌 값이 들어오면 전체 보기
-			}
+			} catch(Exception e) {}
 		}
 
+		// (2) 검색어
+	    String searchWord = request.getParameter("q");
+	    
+	    // (3) 정렬 (추가됨!)
+	    String sortType = request.getParameter("sort");
+	    if(sortType == null || sortType.isEmpty()) {
+	        sortType = "latest"; // 기본값
+	    }
+	    
 		// 페이지 번호 받기
 		String strPageNo = request.getParameter("pageNo");
 		int currentShowPageNo = 1;
@@ -37,13 +44,11 @@ public class IndexController extends AbstractController {
 		if(strPageNo != null) {
 			try {
 				currentShowPageNo = Integer.parseInt(strPageNo);
-			} catch(NumberFormatException e) {
-				currentShowPageNo = 1;
-			}
+			} catch(Exception e) {}
 		}
 		
 		// 전체 게시물 수 계산
-		int totalCount = pdao.getTotalProductCount(categoryNo);
+		int totalCount = pdao.getTotalProductCount(categoryNo, searchWord);
 		
 		int totalPage = (int) Math.ceil((double)totalCount / sizePerPage);
 		
@@ -54,7 +59,7 @@ public class IndexController extends AbstractController {
 		// 상품 리스트 가져오기 (카테고리 번호 전달)
 		List<ProductDTO> productList = null;
 		if(totalCount > 0) {
-			productList = pdao.selectPagingProduct(currentShowPageNo, sizePerPage, categoryNo);
+			productList = pdao.selectPagingProduct(currentShowPageNo, sizePerPage, categoryNo, searchWord, sortType);
 		}
 
 		request.setAttribute("productList", productList);
@@ -62,7 +67,9 @@ public class IndexController extends AbstractController {
 		request.setAttribute("currentPage", currentShowPageNo);
 		
 		request.setAttribute("categoryNo", categoryNo);
-
+		request.setAttribute("searchWord", searchWord);
+	    request.setAttribute("sort", sortType);
+	    
 		// 최신상품 10개 조회 (상단 NEW 섹션용)
 		List<ProductDTO> newProductList = pdao.selectNewProductList();
 		request.setAttribute("newProductList", newProductList);
