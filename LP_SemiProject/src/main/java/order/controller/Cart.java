be2413213b19334edbbc0c69954ad58f1bec5f2c
jjmj188 <jjmj3434@gmail.com>
2026-01-07 15:10:28 +0900

    update joung

--- LP_SemiProject/src/main/java/order/controller/Cart.java
+++ LP_SemiProject/src/main/java/order/controller/Cart.java
@@ -1,13 +1,57 @@
 package order.controller;
 
+import java.util.List;
+
 import common.controller.AbstractController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import member.domain.MemberDTO;
+import order.domain.CartDTO;
+import order.model.*;
 
 public class Cart extends AbstractController {
 
+	private CartDAO cdao = new CartDAO_imple();
+	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String method = request.getMethod();
+		HttpSession session = request.getSession();
+		MemberDTO loginuser = (MemberDTO) session.getAttribute("loginuser");
+		
+		if(loginuser == null) {
+			 request.setAttribute("message", "로그인 후 이용 가능합니다.");
+	         request.setAttribute("loc", request.getContextPath() + "/login/login.lp");
+	         setRedirect(false);
+	         setViewPage("/WEB-INF/msg.jsp");
+	         return;
+		}
+		
+		String userid = loginuser.getUserid();
+		
+		//장바구니 조회
+		List<CartDTO> cartList = cdao.selectCartList(userid);
+		
+		
+		//총 가격, 포인트
+		int sumTotalPrice = 0;
+		int sumTotalPoint = 0;
+		
+		if (cartList != null) {
+			for (CartDTO dto : cartList) {
+	            sumTotalPrice += dto.getTotalPrice();
+	            sumTotalPoint += dto.getTotalPoint();
+	        }
+		}
+		
+		
+		
+		request.setAttribute("cartList", cartList);
+		request.setAttribute("sumTotalPrice", sumTotalPrice);
+		request.setAttribute("sumTotalPoint", sumTotalPoint);
+		
 		setRedirect(false);
 	    setViewPage("/WEB-INF/order/cart.jsp");
 		
--- LP_SemiProject/src/main/java/order/model/CartDAO.java
+++ LP_SemiProject/src/main/java/order/model/CartDAO.java
@@ -1,10 +1,16 @@
 package order.model;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import order.domain.CartDTO;
 
 public interface CartDAO {
 
 	// DB 처리 (있으면 update, 없으면 insert)
 	int addCart(String loginuserid, int productno, int qty)throws SQLException;
+
+	//장바구니 조회
+	List<CartDTO> selectCartList(String userid)throws SQLException;
 	
 }
