20f5d3d870295552995c2e79d7dbd03b0e20f818
jjmj188 <jjmj3434@gmail.com>
2026-01-11 16:03:49 +0900

    update joung

--- LP_SemiProject/src/main/java/order/model/OrderDAO_imple.java
+++ LP_SemiProject/src/main/java/order/model/OrderDAO_imple.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.Context;
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 
 import order.domain.CartDTO;
 import order.domain.OrderDTO;
+import order.domain.OrderDetailDTO;
 import util.security.AES256;
 import util.security.SecretMyKey;
 
@@ -188,4 +190,191 @@ public class OrderDAO_imple implements OrderDAO {
             close();
         }
     }
+    
+  //주문목록(대표상품/총수량/배송정보 포함)
+    @Override
+    public List<OrderDTO> selectMyOrderList(String userid) throws Exception {
+
+        List<OrderDTO> list = new ArrayList<>();
+
+        try {
+            conn = ds.getConnection();
+
+            String sql =
+                " select o.orderno, to_char(o.orderdate,'yyyy.mm.dd') as orderdate, "
+              + "        o.totalprice, o.totalpoint, o.usepoint, "
+              + "        nvl(d.deliverystatus,'배송준비중') as deliverystatus, "
+              + "        d.delivery_company as deliveryCompany, "
+              + "        d.invoice_no as invoiceNo, "
+              + "        s.totalQty, s.itemCount, "
+              + "        r.fk_productno as repProductno, p.productname as repProductname, p.productimg as repProductimg "
+              + " from tbl_order o "
+              + " left join tbl_delivery d "
+              + "   on d.fk_orderno = o.orderno "
+              + " join ( "
+              + "   select fk_orderno, sum(qty) as totalQty, count(*) as itemCount "
+              + "   from tbl_orderdetail "
+              + "   group by fk_orderno "
+              + " ) s "
+              + "   on s.fk_orderno = o.orderno "
+              + " join ( "
+              + "   select od.fk_orderno, od.fk_productno "
+              + "   from tbl_orderdetail od "
+              + "   where od.orderdetailno = ( "
+              + "     select min(od2.orderdetailno) "
+              + "     from tbl_orderdetail od2 "
+              + "     where od2.fk_orderno = od.fk_orderno "
+              + "   ) "
+              + " ) r "
+              + "   on r.fk_orderno = o.orderno "
+              + " join tbl_product p "
+              + "   on p.productno = r.fk_productno "
+              + " where o.fk_userid = ? "
+              + " order by o.orderno desc ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userid);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                OrderDTO dto = new OrderDTO();
+
+                dto.setOrderno(rs.getInt("orderno"));
+                dto.setUserid(userid);
+                dto.setOrderdate(rs.getString("orderdate"));
+
+                dto.setTotalprice(rs.getInt("totalprice"));
+                dto.setTotalpoint(rs.getInt("totalpoint"));
+                dto.setUsepoint(rs.getInt("usepoint"));
+
+                dto.setDeliverystatus(rs.getString("deliverystatus"));
+                dto.setDeliveryCompany(rs.getString("deliveryCompany"));
+                dto.setInvoiceNo(rs.getString("invoiceNo"));
+
+                dto.setTotalQty(rs.getInt("totalQty"));
+                dto.setItemCount(rs.getInt("itemCount")); // moreCount 자동 계산됨
+
+                dto.setRepProductno(rs.getInt("repProductno"));
+                dto.setRepProductname(rs.getString("repProductname"));
+                dto.setRepProductimg(rs.getString("repProductimg"));
+
+                list.add(dto);
+            }
+
+        } finally {
+            close();
+        }
+
+        return list;
+    }
+    
+  //주문별 상세 목록 붙이기
+    @Override
+    public List<OrderDetailDTO> selectMyOrderDetailList(int orderno, String userid) throws Exception {
+
+        List<OrderDetailDTO> list = new ArrayList<>();
+
+        try {
+            conn = ds.getConnection();
+
+            String sql =
+                " select od.orderdetailno, od.fk_orderno as orderno, od.fk_productno as productno, "
+              + "        od.qty, od.unitprice, (od.qty * od.unitprice) as lineprice, "
+              + "        p.productname, p.productimg "
+              + " from tbl_orderdetail od "
+              + " join tbl_order o "
+              + "   on o.orderno = od.fk_orderno "
+              + " join tbl_product p "
+              + "   on p.productno = od.fk_productno "
+              + " where od.fk_orderno = ? "
+              + "   and o.fk_userid = ? "
+              + " order by od.orderdetailno asc ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, orderno);
+            pstmt.setString(2, userid);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                OrderDetailDTO dto = new OrderDetailDTO();
+
+                dto.setOrderdetailno(rs.getInt("orderdetailno"));
+                dto.setOrderno(rs.getInt("orderno"));
+                dto.setProductno(rs.getInt("productno"));
+                dto.setQty(rs.getInt("qty"));
+                dto.setUnitprice(rs.getInt("unitprice"));
+                dto.setLineprice(rs.getInt("lineprice"));
+
+                dto.setProductname(rs.getString("productname"));
+                dto.setProductimg(rs.getString("productimg"));
+
+                list.add(dto);
+            }
+
+        } finally {
+            close();
+        }
+
+        return list;
+    }
+
+    //주문주소 + 배송정보(송장) 조회
+	@Override
+	public OrderDTO selectTrackingInfo(int orderno, String userid) throws Exception {
+		
+		 OrderDTO dto = null;
+
+		    try {
+		        conn = ds.getConnection();
+
+		        String sql =
+		            " select o.orderno, "
+		          + "        o.postcode, o.address, o.detailaddress, o.extraaddress, "
+		          + "        o.deliveryrequest, "
+		          + "        nvl(d.sender_name,'VINYST') as senderName, "
+		          + "        nvl(d.delivery_company,'CJ대한통운') as deliveryCompany, "
+		          + "        nvl(d.deliverystatus,'배송준비중') as deliverystatus, "
+		          + "        d.invoice_no as invoiceNo, "
+		          + "        to_char(d.shipped_date,'yyyy.mm.dd') as shippedDate, "
+		          + "        to_char(d.delivered_date,'yyyy.mm.dd') as deliveredDate "
+		          + " from tbl_order o "
+		          + " left join tbl_delivery d "
+		          + "   on d.fk_orderno = o.orderno "
+		          + " where o.orderno = ? "
+		          + "   and o.fk_userid = ? ";
+
+		        pstmt = conn.prepareStatement(sql);
+		        pstmt.setInt(1, orderno);
+		        pstmt.setString(2, userid);
+
+		        rs = pstmt.executeQuery();
+
+		        if (rs.next()) {
+		            dto = new OrderDTO();
+		            dto.setOrderno(rs.getInt("orderno"));
+		            dto.setUserid(userid);
+
+		            dto.setPostcode(rs.getString("postcode"));
+		            dto.setAddress(rs.getString("address"));
+		            dto.setDetailaddress(rs.getString("detailaddress"));
+		            dto.setExtraaddress(rs.getString("extraaddress"));
+		            dto.setDeliveryrequest(rs.getString("deliveryrequest"));
+
+		            dto.setSenderName(rs.getString("senderName"));
+		            dto.setDeliveryCompany(rs.getString("deliveryCompany"));
+		            dto.setDeliverystatus(rs.getString("deliverystatus"));
+		            dto.setInvoiceNo(rs.getString("invoiceNo"));
+		            dto.setShippedDate(rs.getString("shippedDate"));
+		            dto.setDeliveredDate(rs.getString("deliveredDate"));
+		        }
+
+		    } finally {
+		        close();
+		    }
+
+		    return dto;
+		
+	}
+
+	
 }
--- LP_SemiProject/src/main/webapp/WEB-INF/my_info/my_order.jsp
+++ LP_SemiProject/src/main/webapp/WEB-INF/my_info/my_order.jsp
@@ -4,6 +4,9 @@
     String ctxPath = request.getContextPath();
 %>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
 <link rel="stylesheet" href="<%= ctxPath%>/css/my_info/mypage_layout.css">
 <link rel="stylesheet" href="<%= ctxPath%>/css/my_info/my_inquiry_list.css">
 <link rel="stylesheet" href="<%= ctxPath%>/css/my_info/my_order.css">
@@ -27,91 +30,95 @@
 
       <div class="order-list">
 
-        <div class="order-card">
-          <div class="order-summary">
+        <c:if test="${empty orderList}">
+          <div style="padding:20px;">구매내역이 없습니다.</div>
+        </c:if>
 
-            <div class="order-product">
-              <img src="../images/지수.png" alt="대표 상품">
-              <div class="product-info">
-                <p class="name">
-                  상품명 : 로제 LP
-                  <span class="more-count">외 2건</span>
-                </p>
-                <p class="meta">총 수량 : 3개</p>
-                <p class="price">총 가격 : 500,000원</p>
-              </div>
-            </div>
+        <c:forEach var="o" items="${orderList}">
+          <div class="order-card">
+            <div class="order-summary">
 
-            <div class="order-point">
-              <span>적립포인트: 30p</span>
-              <button class="btn-toggle" type="button" onclick="toggleOrderDetails(this)">
-                펼치기 ▼
-              </button>
-            </div>
-
-            <div class="order-status">
-              <p class="date">2025.01.10</p>
-              <p class="status-text">배송완료</p>
-
-              <!-- 배송완료가 되었을 시 송장번호 확인 팝업 버튼 -->
-              
-	              <button class="btn-toggle"
-                		type="button"
-                		onclick="openTrackingPopup(this)">
-                		송장번호 확인하러가기
-            		</button>
-        		 
-            </div>
-
-          </div>
-
-		<!-- 주문자 정보 및 상품 -->
-          <div class="order-details" aria-hidden="true">
-            <div class="detail-list">
-
-              <div class="order-shipping">
-                <h4>배송 정보</h4>
-                <p class="receiver">받는 사람: 홍길동</p>
-                <p class="address">
-                  우편번호:121212<br>
-                  주소: 서울시 강남구 테헤란로 123, 101동 202호
-                </p>
-              </div>
-              
-			<!-- 상품 -->
-              <div class="detail-item">
-                <img src="../images/지수.png" alt="상품">
-                <div class="detail-info">
-                  <p class="d-name">로제 LP</p>
-                  <p class="d-sub">수량: 1개</p>
+              <div class="order-product">
+                <img src="<%= ctxPath%>${o.repProductimg}" alt="대표 상품">
+                <div class="product-info">
+               
+                <div class="product-name">
+                  <p class="name">
+                    상품명 : ${o.repProductname}
+                  </p>
+ 					<c:if test="${o.moreCount > 0}">
+                      <span class="more-count">외 ${o.moreCount}건</span>
+                    </c:if>
                 </div>
-                <div class="detail-actions">
-                  <div class="detail-price">300,000원</div>
-                  <button
-                    class="btn-review"
-                    type="button"
-                    onclick="openReviewPopup('ORD001','PRD001','로제 LP')">
-                    리뷰쓰기
-                  </button>
+                    
+                  <p class="meta">총 수량 : ${o.totalQty}개</p>
+
+                  <p class="price">
+                    총 가격 :
+                    <fmt:formatNumber value="${o.totalprice}" pattern="#,###"/>원
+                  </p>
                 </div>
               </div>
 
+              <div class="order-point">
+                <span>적립포인트: <fmt:formatNumber value="${o.totalpoint}" pattern="#,###"/>p</span>
+                <button class="btn-toggle" type="button" onclick="toggleOrderDetails(this)">
+                  펼치기 ▼
+                </button>
+              </div>
+
+              <div class="order-status">
+                <p class="date">${o.orderdate}</p>
+                <p class="status-text">${o.deliverystatus}</p>
+
+                 <button class="btn-toggle"
+          			type="button"
+          			data-orderno="${o.orderno}"
+          			onclick="openTrackingPopup(this)">
+    					배송지 & 송장번호
+  					</button>
+               
+              </div>
+
+            </div>
+
+            <div class="order-details" aria-hidden="true">
+              <div class="detail-list">
+
+                <c:forEach var="d" items="${o.orderDetailList}">
+                  <div class="detail-item">
+                    <img src="<%= ctxPath%>${d.productimg}" alt="상품">
+                    <div class="detail-info">
+                      <p class="d-name">${d.productname}</p>
+                      <p class="d-sub">수량: ${d.qty}개</p>
+                    </div>
+
+                    <div class="detail-actions">
+                      <div class="detail-price">
+                        <fmt:formatNumber value="${d.lineprice}" pattern="#,###"/>원
+                      </div>
+
+                      <button class="btn-review"
+                              type="button"
+                              onclick="openReviewPopup('${o.orderno}','${d.productno}','${d.productname}')">
+                        리뷰쓰기
+                      </button>
+                    </div>
+                  </div>
+                </c:forEach>
+
+              </div>
             </div>
           </div>
-        </div>
-        
-        
+        </c:forEach>
 
-		<!-- 페이징바 -->
-        <div class="pagebar">
-          <button class="page-btn prev" disabled><i class="fa-solid fa-chevron-left"></i></button>
-          <button class="page-num active">1</button>
-          <button class="page-num">2</button>
-          <button class="page-num">3</button>
-          <button class="page-num">4</button>
-          <button class="page-btn next"><i class="fa-solid fa-chevron-right"></i></button>
-        </div>
-
+	        <!-- 페이징바 -->
+	        <div class="pagebar">
+			  <ul class="pagination" style="margin:0;">
+			    ${requestScope.pageBar}
+			  </ul>
+		  </div> 
+		  
       </div>
     </section>
   </div>
@@ -121,10 +128,3 @@
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="<%= ctxPath%>/js/my_info/my_order.js"></script>
-
-<script type="text/javascript">
-  
-  
-
-  
-</script>
